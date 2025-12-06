0e996215e6410072205988a466570af7bc7788ec
tanzeel-khan1 <tanzeel0680@gmail.com>
2025-12-06 11:29:51 +0500

    li

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthProvider.jsx";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-  <AuthProvider>
-    <App />
-  </AuthProvider>
- </BrowserRouter>
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
